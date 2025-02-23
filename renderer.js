01b8d242fb96aa183582ff1c0d9aa3deae6cf522
Artem Mamitov <90879703+Impirs@users.noreply.github.com>
2025-02-23 20:20:32 +0100

    Update 23.02

--- renderer.js
+++ renderer.js
@@ -1,3 +1,21 @@
+function goCatalog() {
+    window.location.href = 'page_catalog.html';
+}
+function goAchievements() {
+    window.location.href = 'page_achievements.html';
+}
+function goSettings() {
+    window.location.href = 'page_settings.html';
+}
+
+function goBack() {
+    window.location.href = 'index.html'
+}
+
+function goHome() {
+    window.location.href = 'index.html'
+}
+
 document.getElementById('play_bnt').addEventListener('click', () => {
   alert("Start to Play!")
 });
